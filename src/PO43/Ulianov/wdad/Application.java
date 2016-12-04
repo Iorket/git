ec522b598409c20af813288eb69340fe6d495642
Iorket <jazscripts@gmail.com>
2016-12-04 13:42:09 +0400

    Its first commit

---
+++ src/PO43/Ulianov/wdad/Application.java
@@ -0,0 +1,11 @@
+package PO43.Ulianov.wdad;
+
+/**
+ * Created by Iorket on 04.12.2016.
+ */
+public class Application {
+    public static
+    void main(String[] args){
+        System.out.println("I`m Dmitry Ulianov and I`m not a Monkey ");
+    }
+}
