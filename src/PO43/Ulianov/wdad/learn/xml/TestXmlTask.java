f1f396ae63b01905d69e7280e927e7cdb65429aa
Iorket <jazscripts@gmail.com>
2016-12-06 22:04:38 +0400

    Its first commit

--- src/PO43/Ulianov/wdad/learn/xml/TestXmlTask.java
+++ src/PO43/Ulianov/wdad/learn/xml/TestXmlTask.java
@@ -7,11 +7,17 @@ public class TestXmlTask {
 
     public static void main(String argv[])
     {
+        User tutu=new User("tutu","tutu@mail.ru");
         User peperoni=new User("Peperoni","Peperoni@mail.ru");
         XmlTask Test1= new XmlTask("C:\\Users\\Iorket\\IdeaProjects\\start ing - monkey - to - human - path\\src\\PO43\\Ulianov\\wdad\\learn\\xml\\xmlForATest.xml");
-        System.out.print(Test1.getNoteText("Third note",peperoni));
-        Test1.updateNote("Third note",peperoni,"It`s working");
+        Test1.updateNote("Third note",peperoni,"It`s working/sometimes");
         Test1.setPrivileges("Third note",peperoni,1);
+         //ОШИБКИ
+        //неверное название тайтла
+        System.out.print(Test1.getNoteText("Thir note",peperoni));
+        //отстутствие  тега text в note
+        Test1.updateNote("Ta-da",tutu,"Some  new text");
+        //надо ли это прописывать проверку для других тегов?
 
 
     }
--- src/PO43/Ulianov/wdad/learn/xml/XmlTask.java
+++ src/PO43/Ulianov/wdad/learn/xml/XmlTask.java
@@ -9,20 +9,19 @@ import javax.xml.transform.TransformerFactory;
 import javax.xml.transform.dom.DOMSource;
 import javax.xml.transform.stream.StreamResult;
 import java.io.*;
-//tagName в текстметод
 /**
  * Created by Iorket on 04.12.2016.
  */
 public class XmlTask {
     Document doc;
-    String xmlFile;
+    String xmlFileName;
     XmlTask(String xmlFile){
         try{
-            this.xmlFile=xmlFile;
+            this.xmlFileName =xmlFile;
             File inputFile = new File(xmlFile);
             DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
             DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
-             doc = dBuilder.parse(inputFile);
+            doc = dBuilder.parse(inputFile);
         }
         catch (Exception e)
         {
@@ -30,61 +29,81 @@ public class XmlTask {
         }
     }
     public String getNoteText(String title,User owner) {
-        return TextElement(title, owner).getTextContent();
+        Element currentNote= noteWithTitle(title);
+        if(isNoteOwner(currentNote,owner)) return currentNote.getElementsByTagName("text").item(0).getTextContent();
+        return "";
     }
     public void updateNote(String title,User owner,String text){
-        TextElement(title,owner).setTextContent(text);
-        writeToFile();
-        System.out.print("Well dobe!");
+        Element currentNote= noteWithTitle(title);
+        if(isNoteOwner(currentNote,owner)) {
+            try {
+                currentNote.getElementsByTagName("text").item(0).setTextContent(text);
+            }
+            catch (NullPointerException e) { Element noteTextTag=doc.createElement("text");
+                currentNote.appendChild(noteTextTag).setTextContent(text);}
+            writeToFile();
+            System.out.println("Well done!+-");
+        }
+        else System.out.println("something wrong,I suppose");
     }
     public void setPrivileges(String noteTitle,User user,int newRigths) {
-        //упростить бы
-        NodeList noteList = doc.getElementsByTagName("note");
-        for (int noteCounter = 0; noteCounter < noteList.getLength(); noteCounter++) {
-            Element note = (Element) noteList.item(noteCounter);
-            if (note.getElementsByTagName("title").item(0).getTextContent().equals(noteTitle)) {
-                Element notePrivileges=(Element)note.getElementsByTagName("privileges").item(0);
+                Element notePrivileges=(Element) noteWithTitle(noteTitle).getElementsByTagName("privileges").item(0);
                 NodeList users= notePrivileges.getElementsByTagName("user");
                 for(int usersCount=0;usersCount<users.getLength();usersCount++)
                 {
                     Element privilegeUser=(Element)users.item(usersCount);
-                    //Вопросец:setAttribute и setTextContent не работают
                     if(user.getMail().equals(privilegeUser.getAttribute("mail").toString()))
                     {
                         privilegeUser.getParentNode().removeChild(privilegeUser);
                     }
                 }
                 //надо написать проверку на имя,навеерн
-                if( newRigths!=0) {
+                if( newRigths!=0 ) {
+                    //Вопросец:setAttribute и setTextContent не работают при смене прав RW на R
                     Element newUser = doc.createElement("user");
                     newUser.setAttribute("mail",user.getMail());
+                    newUser.setAttribute("name",user.getName());//может же быть ситуация когда имя в записи отстутсвует?добавить конструктор там на его отстутствие,проверку добавить
                     if(newRigths==2) newUser.setAttribute("rights","RW");
                     else if(newRigths==1) newUser.setAttribute("rights","R");
+                    else System.out.println("Incorect rights,I removed this user HAHA(It`s bad,I know)");
                     notePrivileges.appendChild(newUser);
                 }
-                break;
-
-            }
-        }
         writeToFile();
     }
-    private Element TextElement(String title, User owner){
+    private Element noteWithTitle(String title){
          NodeList noteList=doc.getElementsByTagName("note");
         for(int noteCounter=0;noteCounter<noteList.getLength();noteCounter++) {
             Element note = (Element) noteList.item(noteCounter);
             if (note.getElementsByTagName("title").item(0).getTextContent().equals(title)) {
-                Element noteOwner = (Element) note.getElementsByTagName("owner").item(0);
-                if (noteOwner.getAttribute("name").equals(owner.getName()) && noteOwner.getAttribute("mail").equals(owner.getMail())) {
-                    return (Element) note.getElementsByTagName("text").item(0);
-                }
-            }
+               return note;}
         }
         return null;
+    }
+   /*
+    private Element elementWithOwner(Element verifiableElement,User owner)
+    {try {
+        Element noteOwner = (Element) verifiableElement.getElementsByTagName("owner").item(0);
+        if (owner.getMail().equals(noteOwner.getAttribute("mail")) && owner.getName().equals(noteOwner.getAttribute("name")))
+            return verifiableElement;}
+    catch (NullPointerException e)
+    { }
+    return null;
+    }
+    */
+    private boolean isNoteOwner(Element verifiableElement, User owner)
+    {
+        try {
+            Element noteOwner = (Element) verifiableElement.getElementsByTagName("owner").item(0);
+            if (owner.getMail().equals(noteOwner.getAttribute("mail")) && owner.getName().equals(noteOwner.getAttribute("name"))) return true;
+        }
+        catch (NullPointerException e)
+        {}
+        return false;
     }
     private void writeToFile(){
         try {
             Transformer tr = TransformerFactory.newInstance().newTransformer();
-            StreamResult file= new StreamResult(new File(xmlFile));
+            StreamResult file= new StreamResult(new File(xmlFileName));
             tr.transform(new DOMSource(doc),file);
         } catch (TransformerConfigurationException e) {
             e.printStackTrace();
